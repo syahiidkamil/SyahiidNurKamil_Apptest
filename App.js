7a30ffdf28e7086237a28eb36956ac2aa012e998
Syahiid Nur Kamil <v-Syahiid.kamil@btpn.com>
2021-12-03 04:25:16 +0700

    feat(general): [CTC-0000] add react navigation and implement basic of it

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Contacts from './src/Screens/Contacts';
+import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import configureStore from './src/Redux/Store/configureStore';
+import Contacts from './src/Screens/Contacts';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-    <Contacts />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Contacts" component={Contacts} />
+        <Stack.Screen name="Detail" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   </Provider>
 );
 
